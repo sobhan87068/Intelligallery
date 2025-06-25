f27f7644db822f25a4e6c1169dac83941f9e4645
Sobhan Aryan Namazi <sobhan.aryannamazi@snapp.cab>
2025-06-25 13:23:38 +0330

    Add: play icon to video files preview

--- app/src/main/java/ir/sban/intelligallery/presentation/ui/component/StaggeredGalleryGrid.kt
+++ app/src/main/java/ir/sban/intelligallery/presentation/ui/component/StaggeredGalleryGrid.kt
@@ -1,6 +1,7 @@
 package ir.sban.intelligallery.presentation.ui.component
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -12,17 +13,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Scale
+import ir.sban.intelligallery.R
 import ir.sban.intelligallery.domain.model.MediaFile
 
 @Composable
@@ -238,12 +242,24 @@ private fun ImageItem(
         .size(500).data(item.contentUri).scale(Scale.FIT).crossfade(true)
         .allowHardware(false)
         .build()
-    AsyncImage(
-        modifier = modifier,
-        model = imageRequest,
-        contentDescription = null,
-        contentScale = ContentScale.Crop
-    )
+    Box(modifier = modifier) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = imageRequest,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+
+        if (item.isVideo) {
+            Image(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(),
+                painter = painterResource(R.drawable.play_circle_outline),
+                contentDescription = null
+            )
+        }
+    }
 }
 
 enum class StaggeringType {
